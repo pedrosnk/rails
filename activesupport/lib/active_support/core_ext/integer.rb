d16606d859943d11acefa4a81946092612664dee
Pedro Medeiros <pedrosnk@gmail.com>
2024-04-16 11:05:11 -0400

    Ensure deprecator is loaded on core_ext/integer
    requiring core_ext/integer fails because deprecator is depending here. the deprecator should be required to ensure the safe usage of the core_ext/integer module

--- activesupport/lib/active_support/core_ext/integer.rb
+++ activesupport/lib/active_support/core_ext/integer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "active_support/deprecator"
 require "active_support/core_ext/integer/multiple"
 require "active_support/core_ext/integer/inflections"
 require "active_support/core_ext/integer/time"
